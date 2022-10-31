b4e9fd577a649ac922d65a786216cb9e1ddf643f
kureta <skureta@gmail.com>
2022-10-31 22:34:30 +0300

    First hypothesis testing

--- Makefile
+++ Makefile
@@ -4,7 +4,12 @@ docs_push:
 	poetry run mike deploy --push --update-aliases $(VERSION) latest
 
 test:
+	poetry run pytest --doctest-modules --hypothesis-show-statistics
+
+coverage_run:
 	poetry run coverage run -m pytest --doctest-modules
 
-coverage:
-	poetry run coverage html && xdg-open htmlcov/index.html
+coverage_report:
+	poetry run coverage report
+
+coverage: coverage_run coverage_report
--- src/tests/test_preprocessors.py
+++ src/tests/test_preprocessors.py
@@ -1,14 +1,34 @@
+import numpy as np
 import torch
+from hypothesis import Verbosity, given, settings
+from hypothesis import strategies as st
+from hypothesis.extra.numpy import array_shapes, arrays
 
 from myddsp.preprocessors import get_centered_frames, get_frames
 
+settings.register_profile("ci", max_examples=1000)
+settings.register_profile("dev", max_examples=10)
+settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
 
-def test_get_frames() -> None:
-    y = torch.randn(8, 2, 48000)
+upper = np.finfo("float32").max
+lower = np.finfo("float32").min
+
+
+@given(
+    arrays(
+        dtype=np.float32,
+        shape=(8, 2, 48000),
+        elements=st.floats(lower, upper, allow_nan=False, allow_infinity=False, width=32),
+    )
+)
+def test_get_frames(data: np.array) -> None:
+    y = torch.from_numpy(data)
+    length = y.shape[-1]
+
     frames = get_frames(y, window_length=1024, hop_length=128)
-
     n_frames = frames.shape[-1]
-    calc_n_frames = (48000 - 1024) // 128 + 1
+
+    calc_n_frames = (length - 1024) // 128 + 1
 
     assert n_frames == calc_n_frames
 
